0371bdd41bcefb9ccdd7dd732cd696624ab10d54
atharva <atharva.risbud@gmail.com>
2023-10-30 21:23:10 -0400

    organised code into components, implemented gradcam for explainability

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class MyTansforms():
         return self.traintransforms
     def shadow_test_transform(self,):
         self.testtransforms = transforms.Compose([
+            transforms.Resize(256),
             transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),
         ])
         return self.testtransforms
@@ -27,7 +28,10 @@ class MyTansforms():
    
 class MyDataset(pl.LightningModule):
 
-    def __init__(self,train_df,val_df,test_df,color_channel,shadow_channel,theDataLoader):
+    def __init__(self,train_df,val_df,test_df,color_channel,shadow_channel,theDataLoader, mount_point, batch_size, num_workers):
+        """
+        theDataLoader: describes whether to load the four_channel or 3_channel
+        """
         super().__init__()
 
         self.train_df = train_df
@@ -40,35 +44,54 @@ class MyDataset(pl.LightningModule):
         # self.data_manager = MyConfig()
         self.color_channel = color_channel
         self.shadow_channel = shadow_channel
+        self.mount_point = mount_point
 
-        self.num_workers = 16 #TODO
-        self.batch_size = 64
+        self.num_workers = num_workers #TODO
+        self.batch_size = batch_size
 
         self.transforms = MyTansforms()
         self.transforms_train = self.transforms.shadow_training_transform()
-        self.MyDataLoading = theDataLoader
+        self.transforms_test = self.transforms.shadow_test_transform()
+        if theDataLoader == "three_channel":
+            print("ENTERED 3CHANNEL")
+            self.MyDataLoading = MyDataLoading_color
+        if theDataLoader == "four_channel":
+            print("ENTERED 4CHANNEL")
+            self.MyDataLoading = MyDataLoading_four_channel
 
     def split_xy(self,df,default_string):
         colx = df[self.column_x]
+        print(colx)
         x = colx.apply(lambda x: default_string + x if isinstance(x, str) else x)
         return x,df[self.column_y]
+    
+    def setup(self, stage: str):
+
+        print("SELF")
+        # self.trainX, self.trainY = self.split_xy(self.train_df)
+        # self.train_data = self.MyDataLoading(self.trainX, self.trainY, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_train )
+
+        # self.valX, self.valY = self.split_xy(self.val_df)
+        # self.val_data = self.MyDataLoading(self.valX, self.valY, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_test )
+
+        # self.testX, self.testY = self.split_xy(self.test_df)
+        # self.test_data = self.MyDataLoading(self.testX, self.testY, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_test )
+        pass
    
     def train_dataloader(self):
+        # self.trainX, self.trainY = self.split_xy(self.train_df, "/home/atharva/Abha/Shadows/images_rendered_try2/")
+        self.train_data = self.MyDataLoading(self.train_df, self.mount_point, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_train )
         return DataLoader(self.train_data, batch_size= self.batch_size, shuffle=True, num_workers=self.num_workers)#self.data_manager.get_train_dataloader()
         #,last_drop = True, prefetch_factor = 2
     def val_dataloader(self):
+        # self.valX, self.valY = self.split_xy(self.val_df, "/home/atharva/Abha/Shadows/images_rendered_try2/")
+        self.val_data = self.MyDataLoading(self.val_df, self.mount_point, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_test )
         return DataLoader(self.val_data, batch_size= self.batch_size, shuffle=False, num_workers=self.num_workers)#self.data_manager.get_train_dataloader() #self.data_manager.get_validation_dataloader()
 
     def test_dataloader(self):
+        # self.testX, self.testY = self.split_xy(self.test_df, "/home/atharva/Abha/Shadows/images_rendered_try2/") #TODO
+        self.test_data = self.MyDataLoading(self.test_df, self.mount_point, color_channel = self.color_channel, shadow_channel =self.shadow_channel, transforms =  self.transforms_test )
         return DataLoader(self.test_data, batch_size= self.batch_size, shuffle=False, num_workers=self.num_workers)
 
 
-    def setup(self, stage: str) ->None:
-        self.trainX, self.trainY = self.split_xy(self.train_df)
-        self.train_data = self.MyDataLoading(self.trainX, self.trainY, color_channel = self.color_channel, shadow_channel =self.shadow_channel,mode = "train" , transforms =  self.transforms_train )
-
-        self.valX, self.valY = self.split_xy(self.val_df)
-        self.val_data = self.MyDataLoading(self.valX, self.valY, color_channel = self.color_channel, shadow_channel =self.shadow_channel,mode = "train" , transforms =  self.transforms_train )
-
-        self.testX, self.testY = self.split_xy(self.test_df)
-        self.test_data = self.MyDataLoading(self.testX, self.testY, color_channel = self.color_channel, shadow_channel =self.shadow_channel,mode = "train" , transforms =  self.transforms_train )
+    
